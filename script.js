b96c6cc35ebe97d8a5080f14ce197e75629233f7
Anuj Kumar Singh <anujsingh120802@gmail.com>
2022-11-08 13:25:34 +0530

    CV/Resume

---
+++ script.js
@@ -0,0 +1,28 @@
+function addNewweField() {
+    let newNode=document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows",3);
+    newNode.setAttribute("placeholder","Enter here");
+
+    let weOb=document.getElementById("we");
+    let weAddButtonOb=document.getElementById("weAddButton");
+
+    weOb.insertBefore(newNode,weAddButtonOb);
+}
+
+function addNewaqField(){
+    let newNode=document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("aqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows",3);
+    newNode.setAttribute("placeholder","Enter here");
+
+    let aqOb=document.getElementById("aq");
+    let aqAddButtonOb=document.getElementById("aqAddButton");
+
+    aqOb.insertBefore(newNode,aqAddButtonOb);
+
+}
